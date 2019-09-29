89d0d09cd91343ebabd4e106c80fc7b3a2180f06
VictorHM <rcpoetics@gmail.com>
2019-09-29 17:05:26 +0200

    Clean comments

--- moverPDF.py
+++ moverPDF.py
@@ -11,8 +11,6 @@ import os, shutil, re
 # finalPath: final destination where to copy the files
 # fileFormat: file format of the files we want to copy to destination
 
-#TODO: hacer la función recursiva para poder entrar en todos los directorios que haya
-#la recursividad se basará en si hay o no subdirectorios
 def CopyFiles(origPath, finalPath, fileFormat):
     for foldername, subfolders, filenames in os.walk(origPath):
         for sub in subfolders:
@@ -22,7 +20,8 @@ def CopyFiles(origPath, finalPath, fileFormat):
                         fullpath = os.path.join(foldername, fold, file)
                         shutil.copy(fullpath, finalPath)
 
-
+# TODO This recursive function still doesn't work as expected
+# Work in Progress
 def CopyRecursive(origPath, finalPath, fileFormat):
     for foldername, subfolders, filenames in os.walk(origPath):
         if(subfolders):
@@ -30,30 +29,15 @@ def CopyRecursive(origPath, finalPath, fileFormat):
                 CopyRecursive(subfold, finalPath, fileFormat)
                 
         fullpath = os.path.join(foldername, subfold, file)
-                        shutil.copy(fullpath, finalPath)
-os.chdir(r'D:\Librerias de programas\Calibre')
+        shutil.copy(fullpath, finalPath)
+
+# POINT OF ENTRY
+os.chdir(r'/media/victor/USB/Libros/Calibre Portable/Calibre Library/')
 print('Directorio actual: ' + os.getcwd())
 
 origpath = os.getcwd()
-destpath = r'D:\Librerias de programas\Documentos\Documentos y libros\Programación y tecnología\PythonTest'
+destpath = r'/home/victor/Documents/Libros/'
+print('Starting to gather and copying files...')
 #recorrer cada subdirectorio y obtener el path del archivo PDF
 CopyFiles(origpath, destpath, r'.pdf')
-##for foldername, subfolders, filenames in os.walk(r'D:\Librerias de programas\Calibre'):
-##    print('Foldername ', foldername)
-##    for sub in subfolders:
-##        print('Present subfolder: ' + sub)
-##        for fold, subs, files in os.walk(sub):
-####            print('Folder ')
-####            print(fold)
-####            print('Files ')
-####            print(files)
-####            print(' Subs ')
-####            print(subs)
-####            input("Pulsa una tecla para continuar...")
-##            for file in files:
-##                    if(file.endswith('.pdf')):
-##                        print('Copiando archivo ' + file + ' desde ' + foldername)
-##                        fullpath = os.path.join(foldername, fold, file)
-##                        print('Fullpath: ' + fullpath)
-##                        shutil.copy(fullpath, r'D:\Librerias de programas\Documentos\Documentos y libros\Programación y tecnología\PythonTest')
 print('End')
